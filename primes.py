1128fae31bd922bd44a23f101f8e5c6fa72bc7dc
Israel Flores <ifloresdev@users.noreply.github.com>
2017-03-18 23:31:20 -0600

    Added missing docstrings

--- primes.py
+++ primes.py
@@ -17,9 +17,16 @@ class PrimeClass(object):
         return self._smallest_divisor(num_int) == num_int
 
     def _smallest_divisor(self, num_int):
+        """
+        Find the smallest divisor of an integer number
+        """
         return self._find_divisor(num_int, 2)
 
     def _find_divisor(self, num_int, divisor):
+        """
+        Return the first divisor of an integer number starting from
+        a given number
+        """
         if divisor**2 > num_int:
             return num_int
         if self._divides(divisor, num_int):
@@ -27,4 +34,7 @@ class PrimeClass(object):
         return self._find_divisor(num_int, divisor + 1)
 
     def _divides(self, divisor, num_int):
+        """
+        Test if a number is a divisor of a given number
+        """
         return num_int % divisor == 0
