c21f2f3e6ba5dae3868700202063035fc3046b1c
Israel Flores <ifloresdev@users.noreply.github.com>
2017-03-18 20:37:16 -0600

    py1

--- primes.py
+++ primes.py
@@ -1,16 +1,30 @@
 """
-Your module documentation here
+It provides classes to search for prime numbers
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Contains methods to test for primality
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Test if an integer is prime
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int == 0 or num_int == 1:
+            return False
+        return self._smallest_divisor(num_int) == num_int
+
+    def _smallest_divisor(self, num_int):
+        return self._find_divisor(num_int, 2)
+
+    def _find_divisor(self, num_int, divisor):
+        if divisor**2 > num_int:
+            return num_int
+        if self._divides(divisor, num_int):
+            return divisor
+        return self._find_divisor(num_int, divisor + 1)
+
+    def _divides(self, divisor, num_int):
+        return num_int % divisor == 0
